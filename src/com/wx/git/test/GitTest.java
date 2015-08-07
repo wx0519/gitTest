28282e43cf02b5f21b6398511da5bb9473db6ac9
王祥 <wangxiang@wangxiangdeMacBook-Pro.local>
2015-08-07 11:06:48 +0800

    second commit

--- src/com/wx/git/test/GitTest.java
+++ src/com/wx/git/test/GitTest.java
@@ -4,4 +4,12 @@ package com.wx.git.test;
  * Created by wangxiang on 15/8/7.
  */
 public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("first project");
+
+        System.out.println("second project");
+    }
+
 }
